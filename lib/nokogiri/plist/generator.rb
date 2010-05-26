edad3cfe6a42a0f73abe92a2ee7a85567c025cae
Casey Howard <caseyhoward@caseyhoward.com>
2010-05-26 08:25:10 -0700

    Made some changes to try to make the generator output better, but it's still a work in progress

--- lib/nokogiri/plist/generator.rb
+++ lib/nokogiri/plist/generator.rb
@@ -8,37 +8,41 @@ module Nokogiri
         2
       end
       
-      def self.to_s(value, current_indent = 0)
+      def self.to_s(value, current_indent = 0, do_indent = true)
         case value
           when Array
-            indent(current_indent) + "<array>\n" +
-            value.inject("") { |result, item| result + indent(current_indent) + item.to_plist_xml(current_indent + indent_size) + "\n" } +
-            indent(current_indent) + "</array>"
-          when Hash
-            indent(current_indent) + "\n<dict>\n" +
+            "\n" + indent(current_indent, true) + "<array>" +
             value.inject("") do |result, item| 
               result + 
-              indent(current_indent) + "<key>#{item[0]}</key>" +
-              item[1].to_plist_xml(current_indent + indent_size) + "\n"
-            end +
-            indent(current_indent) + "</dict>"
+              indent(current_indent + indent_size, true) + 
+              item.to_plist_xml(current_indent + indent_size, true)
+            end + "\n" +
+            indent(current_indent, true) + "</array>"
+          when Hash
+            "\n" + indent(current_indent) + "<dict>\n" +
+            value.inject("") do |result, item| 
+              result +
+              indent(current_indent + indent_size, true) + "<key>#{item[0]}</key>" +
+              item[1].to_plist_xml(current_indent + indent_size, false)
+            end + "\n" +
+            indent(current_indent, true) + "</dict>"
           when TrueClass, FalseClass
-            indent(current_indent) + "<#{value}/>\n"
+            indent(current_indent, do_indent) + "<#{value}/>\n"
           when Time
-            indent(current_indent) + "<date>#{value.utc.strftime('%Y-%m-%dT%H:%M:%SZ')}</date>\n"
+            indent(current_indent, do_indent) + "<date>#{value.utc.strftime('%Y-%m-%dT%H:%M:%SZ')}</date>\n"
           when Date # also catches DateTime
-            indent(current_indent) + "<date>#{value.strftime('%Y-%m-%dT%H:%M:%SZ')}</date>\n"
+            indent(current_indent, do_indent) + "<date>#{value.strftime('%Y-%m-%dT%H:%M:%SZ')}</date>\n"
           when String, Symbol
-            indent(current_indent) + "<string>#{value}</string>"
+            indent(current_indent, do_indent) + "<string>#{value}</string>\n"
           when Fixnum, Bignum, Integer
-            indent(current_indent) + "<integer>#{value}</integer>"
+            indent(current_indent, do_indent) + "<integer>#{value}</integer>\n"
           when Float
-            indent(current_indent) + "<float>#{value}</float>"
+            indent(current_indent, do_indent) + "<float>#{value}</float>\n"
         end
       end
       
-      def self.indent(number)
-        " " * number
+      def self.indent(number, do_indent = true)
+        do_indent ? " " * number : "" 
       end
       
     end
@@ -52,8 +56,8 @@ end
   
   klass.class_eval do
     
-    def to_plist_xml(current_indent = 0)
-      Nokogiri::PList::Generator.to_s(self, current_indent)
+    def to_plist_xml(current_indent = 0, do_indent = true)
+      Nokogiri::PList::Generator.to_s(self, current_indent, do_indent)
     end
     
   end
