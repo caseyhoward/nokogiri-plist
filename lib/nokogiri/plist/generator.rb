87e53951f3a3ad81c5e74dafc2356a6b721d9d04
Casey Howard <caseyhoward@caseyhoward.com>
2010-05-27 19:43:51 -0700

    Refactored and fixed crappy tests

--- lib/nokogiri/plist/generator.rb
+++ lib/nokogiri/plist/generator.rb
@@ -8,47 +8,58 @@ module Nokogiri
         2
       end
       
-      def self.to_s(value, current_indent = 0, do_indent=true)
+      def self.to_s(value, current_indent = 0)
         case value
           when Array
-            indent(current_indent) + "<array>\n" +
-            value.inject("") do |result, item|  
-              newline = array_or_hash?(item) ? "\n" : ""
-              indent = current_indent + indent_size           # array_or_hash?(item) ? 0 : current_indent + indent_size              
-              result + item.to_plist_xml_unchomped(indent)
-            end +
-            indent(current_indent) + "</array>\n"
+            tag("array", nil, current_indent) do
+              value.inject("") do |result, item|             
+                result + item.to_plist_xml_unchomped(current_indent + 1)
+              end
+            end
           when Hash
-            indent(current_indent) + "<dict>\n" +
-            value.inject("") do |result, item| 
-              newline = array_or_hash?(item[1]) ? "\n" : ""
-              result +
-              indent(current_indent + indent_size) + "<key>#{item[0]}</key>" +
-              newline +
-              item[1].to_plist_xml_unchomped(current_indent + indent_size, false)
-            end +
-            indent(current_indent) + "</dict>\n"
-          when TrueClass, FalseClass
-            indent(current_indent, do_indent) + "<#{value}/>\n"
+            tag("dict", nil, current_indent) do
+              value.inject("") do |result, item| 
+                result + tag("key", item[0], current_indent + 1).chomp +
+                item[1].to_plist_xml_unchomped
+              end
+            end
+          when TrueClass
+            tag("true", nil, current_indent)
+          when FalseClass
+            tag("false", nil, current_indent)
           when Time
-            indent(current_indent) + "<date>#{value.utc.strftime('%Y-%m-%dT%H:%M:%SZ')}</date>\n"
+            tag("date", value.utc.strftime('%Y-%m-%dT%H:%M:%SZ'), current_indent)
           when Date # also catches DateTime
-            indent(current_indent, do_indent) + "<date>#{value.strftime('%Y-%m-%dT%H:%M:%SZ')}</date>\n"
+            tag("date", value.strftime('%Y-%m-%dT%H:%M:%SZ'), current_indent)
           when String, Symbol
-            indent(current_indent, do_indent) + "<string>#{value}</string>\n"
+            tag("string", value, current_indent)
           when Fixnum, Bignum, Integer
-            indent(current_indent, do_indent) + "<integer>#{value}</integer>\n"
+            tag("integer", value, current_indent)
           when Float
-            indent(current_indent, do_indent) + "<real>#{value}</real>\n"
+            tag("real", value, current_indent)
         end
       end
       
-      def self.indent(number, do_indent=true)
-        do_indent ? " " * number : "" 
+      def self.indent(number)
+        " " * (number * indent_size) 
       end
       
       def self.array_or_hash?(item)
         item.is_a?(Hash) or item.is_a?(Array)
+      end
+      
+      def self.tag(name, content=nil, current_indent=0, &block)
+        return indent(current_indent) + "<#{name}/>\n" unless content or block_given?
+        same_line = (["array", "dict"].include? name) ? false : true
+        closing_tag_indent = same_line ? 0 : current_indent
+        indent(current_indent) + "<#{name}>" + 
+        (same_line ? "" : "\n") +
+        if block_given?
+          yield
+        else
+          content
+        end.to_s +
+        indent(closing_tag_indent) + "</#{name}>\n"
       end
     end
     
@@ -60,12 +71,12 @@ end
   
   klass.class_eval do
 
-    def to_plist_xml(current_indent = 0, do_indent=true)
-      self.to_plist_xml_unchomped(current_indent, do_indent).chomp
+    def to_plist_xml(current_indent = 0)
+      self.to_plist_xml_unchomped(current_indent).chomp
     end
     
-    def to_plist_xml_unchomped(current_indent = 0, do_indent=true)
-      Nokogiri::PList::Generator.to_s(self, current_indent, do_indent)
+    def to_plist_xml_unchomped(current_indent = 0)
+      Nokogiri::PList::Generator.to_s(self, current_indent)
     end
     
   end
--- test/nokogiri/plist/test_generator.rb
+++ test/nokogiri/plist/test_generator.rb
@@ -6,36 +6,36 @@ class Nokogiri::PList::GeneratorTest < Test::Unit::TestCase
       
     should "output arrays correctly" do
       assert_equal(
-        "  <array>\n    <integer>1</integer>\n    <string>2</string>\n    <true/>\n    <real>0.3</real>\n  </array>",
+        "    <array>\n      <integer>1</integer>\n      <string>2</string>\n      <true/>\n      <real>0.3</real>\n    </array>",
         [1, "2", true, 0.3].to_plist_xml(2)
       )  
     end
     
     should "output hashs correctly" do
       assert_equal(
-        "  <dict>\n    <key>one</key><integer>1</integer>\n    <key>two</key><string>2</string>\n    <key>3</key><string>Three</string>\n  </dict>",
+        "    <dict>\n      <key>one</key><integer>1</integer>\n      <key>two</key><string>2</string>\n      <key>3</key><string>Three</string>\n    </dict>",
         { "one" => 1, :two => "2", 3 => "Three" }.to_plist_xml(2)
       )
     end
       
     should "output strings correctly" do
       assert_equal "<string>test</string>", "test".to_plist_xml
-      assert_equal "  <string>test</string>", "test".to_plist_xml(2)
+      assert_equal "    <string>test</string>", "test".to_plist_xml(2)
     end
     
     should "output integers correctly" do
       assert_equal "<integer>123</integer>", 123.to_plist_xml
-      assert_equal "  <integer>234</integer>", 234.to_plist_xml(2)
+      assert_equal "    <integer>234</integer>", 234.to_plist_xml(2)
     end
     
     should "output true correctly" do
       assert_equal "<true/>", true.to_plist_xml
-      assert_equal "  <true/>", true.to_plist_xml(2)
+      assert_equal "    <true/>", true.to_plist_xml(2)
     end
     
     should "output false correctly" do
       assert_equal "<false/>", false.to_plist_xml
-      assert_equal "  <false/>", false.to_plist_xml(2)
+      assert_equal "    <false/>", false.to_plist_xml(2)
     end
     
   end
