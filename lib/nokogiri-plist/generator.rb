a394da1abb284a3dace4e3334bb8767883df98c8
Casey Howard <caseyhoward@caseyhoward.com>
2011-12-10 12:24:13 -0800

    Updated the generator to use the Nokogiri builder instead of manually generating the XML. The options passed are now passed directly to the to_xml method.

--- lib/nokogiri-plist/generator.rb
+++ lib/nokogiri-plist/generator.rb
@@ -4,78 +4,56 @@ module NokogiriPList
 
   class Generator
 
-    def self.indent_size
-      2
-    end
-
-    def self.to_s(value, current_indent = 0)
-      # Todo: make these procs and allow custom procs (for data for example)
+    def self.to_xml(value, xml)
       case value
       when Array
-          tag("array", nil, current_indent) do
-            value.inject("") do |result, item|
-              result + item.to_plist_xml_unchomped(current_indent + 1)
-            end
+        xml.array do
+          value.each do |value|
+            value.to_plist_xml(0, xml)
           end
-        when Hash
-          tag("dict", nil, current_indent) do
-            value.inject("") do |result, (dict_key, dict_value)|
-              result + tag("key", dict_key, current_indent + 1).chomp +
-              dict_value.to_plist_xml_unchomped
-            end
-          end
-        when TrueClass
-          tag("true", nil, current_indent)
-        when FalseClass
-          tag("false", nil, current_indent)
-        when Time
-          tag("date", value.utc.strftime('%Y-%m-%dT%H:%M:%SZ'), current_indent)
-        when Date # also catches DateTime
-          tag("date", value.strftime('%Y-%m-%dT%H:%M:%SZ'), current_indent)
-        when String, Symbol
-          tag("string", value, current_indent)
-        when Fixnum, Bignum, Integer
-          tag("integer", value, current_indent)
-        when Float
-          tag("real", value, current_indent)
-        when BigDecimal
-          tag("real", value.to_s('F'), current_indent)
         end
+      when Hash
+        xml.dict do
+          value.each do |dict_key, dict_value|
+            xml.key dict_key
+            dict_value.to_plist_xml(0, xml)
+          end
+        end
+      when TrueClass
+        xml.true
+      when FalseClass
+        xml.false
+      when Time
+      when Date # also catches DateTime
+      when String, Symbol
+        xml.string value
+      when Fixnum, Bignum, Integer
+        xml.integer value
+      when Float
+        xml.real value
+      when BigDecimal
+        xml.real value.to_s('F')
       end
 
-    def self.indent(number)
-      " " * (number * indent_size)
-    end
-
-    def self.array_or_hash?(item)
-      item.is_a?(Hash) or item.is_a?(Array)
-    end
-
-    def self.tag(name, content=nil, current_indent=0, &block)
-      if content or block_given?
-        new_line = (["array", "dict"].include? name) ? "\n" : ""
-        closing_tag_indent = (new_line != "\n") ? 0 : current_indent
-        indent(current_indent) + "<#{name}>" + new_line +
-        (block_given? ? yield : content).to_s +
-        indent(closing_tag_indent) + "</#{name}>\n"
-      else
-        indent(current_indent) + "<#{name}/>\n"
-      end
     end
   end
-
 end
-
 
 [String, Symbol, Integer, Float, BigDecimal, Date, Time, Hash, Array, TrueClass, FalseClass].each do |klass|
   klass.class_eval do
 
-    def to_plist_xml(current_indent = 0)
-      self.to_plist_xml_unchomped(current_indent).chomp
-    end
-
-    def to_plist_xml_unchomped(current_indent = 0)
-      NokogiriPList::Generator.to_s(self, current_indent)
+    def to_plist_xml(options = {}, xml = nil)
+      if xml
+        NokogiriPList::Generator.to_xml(self, xml)
+      else
+        builder = Nokogiri::XML::Builder.new do |xml|
+          NokogiriPList::Generator.to_xml(self, xml)
+        end
+        xml = builder.to_xml(options)
+        xml.sub!(/^[^\n]*\n/, "")
+        xml.chomp!
+        xml
+      end
     end
 
   end
--- test/nokogiri-plist/test_generator.rb
+++ test/nokogiri-plist/test_generator.rb
@@ -2,57 +2,61 @@ require 'test/test_helper'
 
 class NokogiriPList::GeneratorTest < Test::Unit::TestCase
 
-  context ".to_s" do
+  context ".to_plist_xml" do
 
     should "output arrays correctly" do
-      xml_lines = [1, "2", true, 0.3].to_plist_xml(2).split(/\n/)
+      xml_lines = [1, "2", true, 0.3].to_plist_xml.split(/\n/)
       assert_equal([
-                    "    <array>",
-                    "      <integer>1</integer>",
-                    "      <string>2</string>",
-                    "      <true/>",
-                    "      <real>0.3</real>",
-                    "    </array>"
+                    "<array>",
+                    "  <integer>1</integer>",
+                    "  <string>2</string>",
+                    "  <true/>",
+                    "  <real>0.3</real>",
+                    "</array>"
                    ], xml_lines)
     end
 
     should "output hashs correctly" do
-      xml_lines = { "one" => 1, :two => "2", 3 => "Three" }.to_plist_xml(2).split(/\n/)
-      assert_equal("    <dict>", xml_lines[0])
+      xml_lines = { "one" => 1, :two => "2", 3 => "Three" }.to_plist_xml.split(/\n/)
+      assert_equal("<dict>", xml_lines[0])
       assert_same_elements([
-          "      <key>one</key><integer>1</integer>",
-          "      <key>two</key><string>2</string>",
-          "      <key>3</key><string>Three</string>"
+          "  <key>one</key><integer>1</integer>",
+          "  <key>two</key><string>2</string>",
+          "  <key>3</key><string>Three</string>"
         ],
         xml_lines[1..3]
       )
-      assert_equal("    </dict>", xml_lines[4])
+      assert_equal("</dict>", xml_lines[4])
     end
 
     should "output strings correctly" do
-      assert_equal "<string>test</string>", "test".to_plist_xml
-      assert_equal "    <string>test</string>", "test".to_plist_xml(2)
+      assert_equal "<string>a&amp;b</string>", "a&b".to_plist_xml
     end
 
     should "output integers correctly" do
       assert_equal "<integer>123</integer>", 123.to_plist_xml
-      assert_equal "    <integer>234</integer>", 234.to_plist_xml(2)
     end
 
     should "output true correctly" do
       assert_equal "<true/>", true.to_plist_xml
-      assert_equal "    <true/>", true.to_plist_xml(2)
     end
 
     should "output false correctly" do
       assert_equal "<false/>", false.to_plist_xml
-      assert_equal "    <false/>", false.to_plist_xml(2)
     end
 
     should "output decimal correctly" do
       require 'bigdecimal'
       assert_equal "<real>42.0</real>", BigDecimal.new("42").to_plist_xml
-      assert_equal "    <real>42.0</real>", BigDecimal.new("42").to_plist_xml(2)
+    end
+
+    should "build the document with Nokogiri and pass the options" do
+      options = mock()
+      builder = mock()
+      xml = "<?xml version=\"1.0\"?>\n<test/>"
+      builder.expects(:to_xml).with(options).returns(xml)
+      Nokogiri::XML::Builder.expects(:new).returns(builder)
+      assert_equal "<test/>", [1, 2, 3].to_plist_xml(options)
     end
 
   end
