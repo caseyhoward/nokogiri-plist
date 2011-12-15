fe9cb7f90ef516779c6fbcb27e59d047437072e0
Casey Howard <caseyhoward@caseyhoward.com>
2011-12-14 20:21:46 -0800

    Made generator a singleton class.

--- lib/nokogiri-plist/generator.rb
+++ lib/nokogiri-plist/generator.rb
@@ -1,41 +1,54 @@
 require 'bigdecimal'
+require 'singleton'
 
 module NokogiriPList
 
   class Generator
+    include Singleton
 
-    class << self; attr_reader :generators; end
+    attr_reader :generators
 
-    @generators = {
-      Array => Proc.new do |xml, value|
-        xml.array do
-          value.each do |value|
-            value.to_plist_xml({}, xml)
+    def initialize
+      @generators = {
+        Array => Proc.new do |xml, value|
+          xml.array do
+            value.each do |value|
+              generate(value, {}, xml)
+            end
+          end
+        end,
+        Hash => Proc.new do |xml, value|
+          xml.dict do
+            value.each do |key, value|
+              xml.key key
+              generate(value, {}, xml)
+            end
+          end
+        end,
+        TrueClass  => Proc.new { |xml, value| xml.true },
+        FalseClass => Proc.new { |xml, value| xml.false },
+        Time       => Proc.new { |xml, value| xml.date value.utc.strftime('%Y-%m-%dT%H:%M:%SZ') },
+        Date       => Proc.new { |xml, value| xml.date value.strftime('%Y-%m-%dT%H:%M:%SZ') }, # also catches DateTime
+        String     => Proc.new { |xml, value| xml.string value },
+        Symbol     => Proc.new { |xml, value| xml.string value },
+        Fixnum     => Proc.new { |xml, value| xml.integer value },
+        Bignum     => Proc.new { |xml, value| xml.integer value },
+        Integer    => Proc.new { |xml, value| xml.integer value },
+        Float      => Proc.new { |xml, value| xml.real value },
+        BigDecimal => Proc.new { |xml, value| xml.real value.to_s('F') }
+      }
+
+      @generators.keys.each do |klass|
+        klass.class_eval do
+          def to_plist_xml(options = {})
+            NokogiriPList::Generator.instance.generate(self, options)
           end
         end
-      end,
-      Hash => Proc.new do |xml, value|
-        xml.dict do
-          value.each do |key, value|
-            xml.key key
-            value.to_plist_xml({}, xml)
-          end
-        end
-      end,
-      TrueClass  => Proc.new { |xml, value| xml.true },
-      FalseClass => Proc.new { |xml, value| xml.false },
-      Time       => Proc.new { |xml, value| xml.date value.utc.strftime('%Y-%m-%dT%H:%M:%SZ') },
-      Date       => Proc.new { |xml, value| xml.date value.strftime('%Y-%m-%dT%H:%M:%SZ') }, # also catches DateTime
-      String     => Proc.new { |xml, value| xml.string value },
-      Symbol     => Proc.new { |xml, value| xml.string value },
-      Fixnum     => Proc.new { |xml, value| xml.integer value },
-      Bignum     => Proc.new { |xml, value| xml.integer value },
-      Integer    => Proc.new { |xml, value| xml.integer value },
-      Float      => Proc.new { |xml, value| xml.real value },
-      BigDecimal => Proc.new { |xml, value| xml.real value.to_s('F') }
-    }
+      end
 
-    def self.build_xml(value, xml)
+    end
+
+    def build_xml(value, xml)
       @generators.each do |klass, generator|
         if value.is_a? klass
           generator.call(xml, value)
@@ -44,7 +57,7 @@ module NokogiriPList
       end
     end
 
-    def self.generate(value, options, xml)
+    def generate(value, options, xml=nil)
       if xml
         build_xml(value, xml)
       else
@@ -53,7 +66,7 @@ module NokogiriPList
       end
     end
 
-    def self.build_fragment(value)
+    def build_fragment(value)
       Nokogiri::XML::DocumentFragment.parse("").tap do |document|
         Nokogiri::XML::Builder.with(document) do |xml|
           build_xml(value, xml)
@@ -61,7 +74,7 @@ module NokogiriPList
       end
     end
 
-    def self.build_document(value)
+    def build_document(value)
       Nokogiri::XML::Builder.new do |xml|
         xml.doc.create_internal_subset("plist", "-//Apple Computer//DTD PLIST 1.0//EN", "http://www.apple.com/DTDs/PropertyList-1.0.dtd")
         xml.plist(:version => "1.0") do |xml|
@@ -74,13 +87,4 @@ module NokogiriPList
 
 end
 
-NokogiriPList::Generator.generators.keys.each do |klass|
-  klass.class_eval do
-
-    def to_plist_xml(options = {}, xml = nil)
-      NokogiriPList::Generator.generate(self, options, xml)
-    end
-
-  end
-
-end
+NokogiriPList::Generator.instance # Initialize the singleton instance
